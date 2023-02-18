acc11358230f38eea0d43a4a25da3decea26f2b5
AnujDagade <dagadeanuj@gmail.com>
2023-02-18 11:09:07 +0000

    tt

--- emailTest.php
+++ emailTest.php
@@ -7,7 +7,7 @@ require_once('vendor/autoload.php');
 $client = new \GuzzleHttp\Client();
 
 $response = $client->request('POST', 'https://api.courier.com/send', [
-  'body' => '{"message":{"to":{"email":"dagadeanj@gmail.com","data":"gggyygyygyy"},"content":{"title":"hghgjhg","body":"gvggggh"}}}',
+  'body' => '{"message":{"to":{"email":"dagadeanuj@gmail.com","data":""},"content":{"title":"demo","body":"<b>Last test</b>"}}}',
   'headers' => [
     'Accept' => 'application/json',
     'Authorization' => 'Bearer pk_prod_AHT6G9ABTXMCJMG97ZHVE5AWPZMB',
